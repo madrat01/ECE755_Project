4b00c6c2a0da711ba9d1cff939ad63a0bcd8accb
Alok Ballal <aballal@tux-138.cae.wisc.edu>
2022-03-08 13:02:33 -0600

    Width correction

--- RTL/dnn_gnn.sv
+++ RTL/dnn_gnn.sv
@@ -1,15 +1,15 @@
 module dnn_gnn (
     input logic                 clk, 
-    input logic signed [5:0]    x0, x1, x2, x3, 
+    input logic signed [6:0]    x0, x1, x2, x3, 
     input logic signed [4:0]    w04, w05, w06, w07, w14, w15, w16, w17, w24, w25, w26, w27, w34, w35, w36, w37, w48, w58, w49, w59, w68, w69, w78, w79,
     input logic                 in_ready,
-    input logic signed [13:0]   y4_aggr, y5_aggr, y6_aggr, y7_aggr, 
-    output logic signed [12:0]  y4_relu, y5_relu, y6_relu, y7_relu, 
-    output logic signed [19:0]  out0, out1, 
+    input logic signed [16:0]   y4_aggr, y5_aggr, y6_aggr, y7_aggr, 
+    output logic signed [14:0]  y4_relu, y5_relu, y6_relu, y7_relu, 
+    output logic signed [22:0]  out0, out1, 
     output logic                out0_ready, out1_ready
 );
 
-logic signed [12:0] y4, y5, y6, y7;
+logic signed [14:0] y4, y5, y6, y7;
 logic               out_comp_ready;
 
 always_ff @ (posedge clk) begin
@@ -23,10 +23,10 @@ always_ff @ (posedge clk) begin
         out_comp_ready <= 1'b0;
 end
         
-assign y4_relu = y4[12] ? 0 : y4;
-assign y5_relu = y5[12] ? 0 : y5;
-assign y6_relu = y6[12] ? 0 : y6;
-assign y7_relu = y7[12] ? 0 : y7;
+assign y4_relu = y4[14] ? 0 : y4;
+assign y5_relu = y5[14] ? 0 : y5;
+assign y6_relu = y6[14] ? 0 : y6;
+assign y7_relu = y7[14] ? 0 : y7;
 
 always_ff @ (posedge clk) begin
     if (out_comp_ready) begin
--- RTL/gnn.sv
+++ RTL/gnn.sv
@@ -11,10 +11,10 @@ module gnn (
     input logic signed [4:0]    w48, w58, w68, w78,
     input logic signed [4:0]    w49, w59, w69, w79,
     input logic                 in_ready,
-    output logic signed [19:0]  out0_node0, out1_node0,
-    output logic signed [19:0]  out0_node1, out1_node1,
-    output logic signed [19:0]  out0_node2, out1_node2,
-    output logic signed [19:0]  out0_node3, out1_node3,
+    output logic signed [22:0]  out0_node0, out1_node0,
+    output logic signed [22:0]  out0_node1, out1_node1,
+    output logic signed [22:0]  out0_node2, out1_node2,
+    output logic signed [22:0]  out0_node3, out1_node3,
     output logic                out0_ready_node0, out1_ready_node0, 
     output logic                out0_ready_node1, out1_ready_node1, 
     output logic                out0_ready_node2, out1_ready_node2, 
@@ -27,19 +27,19 @@ module gnn (
 //     \      /
 //      Node 3
 
-logic signed [5:0]  x0_node0_aggr, x1_node0_aggr, x2_node0_aggr, x3_node0_aggr;
-logic signed [5:0]  x0_node1_aggr, x1_node1_aggr, x2_node1_aggr, x3_node1_aggr;
-logic signed [5:0]  x0_node2_aggr, x1_node2_aggr, x2_node2_aggr, x3_node2_aggr;
-logic signed [5:0]  x0_node3_aggr, x1_node3_aggr, x2_node3_aggr, x3_node3_aggr;
+logic signed [6:0]  x0_node0_aggr, x1_node0_aggr, x2_node0_aggr, x3_node0_aggr;
+logic signed [6:0]  x0_node1_aggr, x1_node1_aggr, x2_node1_aggr, x3_node1_aggr;
+logic signed [6:0]  x0_node2_aggr, x1_node2_aggr, x2_node2_aggr, x3_node2_aggr;
+logic signed [6:0]  x0_node3_aggr, x1_node3_aggr, x2_node3_aggr, x3_node3_aggr;
     
-logic signed [13:0]  y4_node0_aggr, y5_node0_aggr, y6_node0_aggr, y7_node0_aggr; 
-logic signed [12:0]  y4_node0, y5_node0, y6_node0, y7_node0;
-logic signed [13:0]  y4_node1_aggr, y5_node1_aggr, y6_node1_aggr, y7_node1_aggr; 
-logic signed [12:0]  y4_node1, y5_node1, y6_node1, y7_node1;
-logic signed [13:0]  y4_node2_aggr, y5_node2_aggr, y6_node2_aggr, y7_node2_aggr; 
-logic signed [12:0]  y4_node2, y5_node2, y6_node2, y7_node2;
-logic signed [13:0]  y4_node3_aggr, y5_node3_aggr, y6_node3_aggr, y7_node3_aggr; 
-logic signed [12:0]  y4_node3, y5_node3, y6_node3, y7_node3;
+logic signed [16:0]  y4_node0_aggr, y5_node0_aggr, y6_node0_aggr, y7_node0_aggr; 
+logic signed [14:0]  y4_node0, y5_node0, y6_node0, y7_node0;
+logic signed [16:0]  y4_node1_aggr, y5_node1_aggr, y6_node1_aggr, y7_node1_aggr; 
+logic signed [14:0]  y4_node1, y5_node1, y6_node1, y7_node1;
+logic signed [16:0]  y4_node2_aggr, y5_node2_aggr, y6_node2_aggr, y7_node2_aggr; 
+logic signed [14:0]  y4_node2, y5_node2, y6_node2, y7_node2;
+logic signed [16:0]  y4_node3_aggr, y5_node3_aggr, y6_node3_aggr, y7_node3_aggr; 
+logic signed [14:0]  y4_node3, y5_node3, y6_node3, y7_node3;
 
 // Aggregated i/p features
 // Aggregate x_inputs
